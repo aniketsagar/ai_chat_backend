18652149b69f3d9ec3a35878675e44c09348300e
aniket <sagar14aniket@gmail.com>
2026-05-26 17:48:04 +0530

    removed pydantic import from prompt service

--- app/prompts/promptService.py
+++ app/prompts/promptService.py
@@ -1,7 +1,7 @@
 # service to create a prompt
-from pydantic import BaseModel
 
-class PromptService(BaseModel):
+
+class PromptService():
   def generatePrompt(self,message:str):
     role = " you are a life assistent with expertise" \
     "in everything"
