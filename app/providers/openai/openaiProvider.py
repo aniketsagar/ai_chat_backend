8cd3d457a8efd8e01234ded730330b61fb3b6549
aniket <sagar14aniket@gmail.com>
2026-05-27 17:23:55 +0530

    added error handling in openai provider,currently handling every error in a umbrella fashion

--- app/providers/openai/openaiProvider.py
+++ app/providers/openai/openaiProvider.py
@@ -1,17 +1,31 @@
 # this class contains the rigging of openai
-from openai import OpenAI
+import openai
+from openai import OpenAI, OpenAIError
 import logging
 logger = logging.getLogger(__name__)
 class OpenAIProvider():
   def __init__(self):
-    self.client = OpenAI(
-    )
+    try:
+      self.client = OpenAI(
+        #api_key ="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"
+      )
+    except Exception as e:
+      logger.info(f"::Failed to create OpenAI object::Error::{e}")
   def processRequest(self,prompt):
     logger.info(prompt)
-    response = self.client.responses.create(
-      model = "gpt-4.1-nano",# this is cheapest
-      input =  prompt["input"],
-      instructions = prompt["instruction"]
-    )
-   
+    response = None 
+    try:
+      
+      response = self.client.responses.create(
+        model = "gpt-4.1-nano",# this is cheapest
+        input =  prompt["input"],
+        instructions = prompt["instruction"]
+      )
+    except OpenAIError as e:
+      response = {
+        "status": 1,
+        "agent_error": e     
+      }
+    logger.info("OpenAI response ##########")
+    logger.info(response)
     return response
