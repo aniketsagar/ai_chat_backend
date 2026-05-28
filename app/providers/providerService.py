8a27c51310011d2cd8ce3aff44d109ab4a043e23
aniket <sagar14aniket@gmail.com>
2026-05-28 14:14:47 +0530

    now making specific provider's object during init

--- app/providers/providerService.py
+++ app/providers/providerService.py
@@ -8,18 +8,26 @@ logger = logging.getLogger(__name__)
 class  ProviderService():
   def __init__(self, model:str):
     self.model = model
+    if (self.model == "gpt"):
+      self.provider = OpenAIProvider()
+    else: 
+        self.provider = None
 
   def generate(self, prompt:dict[str,str]):
     response = None
     logger.info(":::ProviderService:::",vars(self))
-    if (self.model.lower() == "gpt"):
+    
+    if(self.provider):
       try:
-        result = OpenAIProvider().generate(prompt)
+        result = self.provider.generate(prompt)
         response = result.output_text
       except Exception as e:
         logger.info(f"::Exception::{e}")
         response = result
       
       logger.info(":::ProviderService:::")
+      logger.info(response) 
+    else:
+      logger.info(":::ProviderService::: Error:: No provider found")
       logger.info(response) 
     return response
