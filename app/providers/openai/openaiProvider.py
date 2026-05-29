c3a7b8eabaa7cbcbc81c52dfcca5a9b577fdd0ec
aniket <sagar14aniket@gmail.com>
2026-05-29 23:20:14 +0530

    minor change to provider name

--- app/providers/openai/openaiProvider.py
+++ app/providers/openai/openaiProvider.py
@@ -10,7 +10,8 @@ class OpenAIProvider():
     try:
       self.client = OpenAI(
         max_retries=max_retries,
-        timeout=120,)
+        timeout=120,
+      )
     except Exception as e:
       logger.info(f"::Failed to create OpenAI object::Error::{e}")
   def generate(self,prompt) -> OpenAIProviderResult:
@@ -20,7 +21,7 @@ class OpenAIProvider():
     error = None
     error_type = None 
     response = None 
-    provider = "OPENAI"
+    provider = "openai"
     clientResponse = None
     try:
      
