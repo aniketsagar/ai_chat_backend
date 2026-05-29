490860bbf529d97981a1c316a9baa0f635ae9a4d
aniket <sagar14aniket@gmail.com>
2026-05-29 23:16:20 +0530

    changed the chat response type

--- app/api/routes/chat.py
+++ app/api/routes/chat.py
@@ -15,11 +15,37 @@ logger.info("*******************************")
 logger.info("created conversation service object")
 logger.info(service)
 @router.post("/")
-async def process_chat(chat: ChatRequest):
-  generated_response = service.generate(chat.message)
-  res = ChatResponse (
-    conversation_id = chat.conversation_id,
-    status = "ok",
-    response= f"{generated_response["response"]}"
-  )
+async def process_chat(chat: ChatRequest) -> ChatResponse:
+
+  try:
+    generated_response = service.generate(chat.message,chat.conversation_id)
+    logger.info("********************************")
+    logger.info(generated_response)
+    if(generated_response.result):
+      res = ChatResponse (
+        conversation_id = chat.conversation_id,
+        status = "successful",
+        success=True,
+        result= generated_response.result,
+        provider_name=generated_response.provider_name
+      )
+    else:
+       res = ChatResponse (
+        conversation_id = chat.conversation_id,
+        status = "Failed",
+        success=False,
+        error=generated_response.error,
+        error_code=generated_response.error_code,
+        provider_name=generated_response.provider_name
+      )
+  except Exception as e:
+    logger.info(f"::ChatApi::Exception::{e}")
+    res = ChatResponse (
+      conversation_id = chat.conversation_id,
+      status = "Failed",
+      success=False,
+      error="internal server error",
+      error_code="500",
+      provider_name=generated_response.provider_name
+    )
   return res 
