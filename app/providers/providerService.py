b3dd0e2ce4b670dbb1f94e1fb2db42fd2cadac3a
aniket <sagar14aniket@gmail.com>
2026-06-02 22:10:31 +0530

    changes in privider service for streaming response

--- app/providers/providerService.py
+++ app/providers/providerService.py
@@ -4,6 +4,7 @@
 from .openai.openaiProvider import OpenAIProvider
 from ..models.providerService import ProviderServiceResponse
 import logging 
+from contextlib import  contextmanager
 logger = logging.getLogger(__name__)
 class  ProviderService():
   def __init__(self, model:str):
@@ -57,3 +58,37 @@ class  ProviderService():
          provider_name = self.provider_name 
       )
     return response
+
+  @contextmanager
+  def stream(self, prompt:dict[str,str]):
+    response = None
+    logger.info(":::ProviderService:::",vars(self))
+    
+    if(self.provider):
+      try:
+        with self.provider.stream(prompt) as stream:
+          for chunk in stream:
+            yield chunk
+      except Exception as e:
+        logger.info(f"::Exception::{e}")
+        response = ProviderServiceResponse(
+          success = False,
+          error_code = "internal_server_error",
+          error = f"::Exception::{e}",
+          provider_name = self.provider_name 
+        )
+      
+      logger.info(":::ProviderService:::")
+      logger.info(response) 
+    else:
+      logger.info(":::ProviderService::: Error:: No provider found")
+      logger.info(response) 
+      response =   ProviderServiceResponse(
+        success = False,
+        error_code = "internal_server_error",
+        error = f"::Exception::No Provider found",
+         provider_name = self.provider_name 
+      )
+    if(response):
+
+      yield response
